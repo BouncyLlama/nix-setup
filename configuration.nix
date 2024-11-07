36f8f0c09f0b1391089512836bc8fabcea778f15
fryalien <164930880+fryalien@users.noreply.github.com>
2024-11-07 16:59:45 +0100

    y

--- configuration.nix
+++ configuration.nix
@@ -44,11 +44,11 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the SDDM display manager.
   services.displayManager.sddm.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
   # Configure keymap in X11.
   services.xserver.xkb = {
